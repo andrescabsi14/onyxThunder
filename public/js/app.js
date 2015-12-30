be4f0f314ca81133e0fec8c6c139f09325390a27
AndresCabrera <andrescabsi@gmail.com>
2015-12-30 02:51:39 -0500

    First Controller

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+(function(){
+	var app = angular.module('r07', []);
+
+	app.controller('R07Controller', function(){
+		this.note = {
+			id: 001,
+			date: 'December 29 - 2015',
+			title: 'My entry title',
+			book: ['John 14:19-20', 'Mathew 14:19-20'],
+			body: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget urna dui. Donec a malesuada ligula, a tempor lorem. In at ante porttitor, molestie tortor eget, eleifend arcu. In iaculis ornare felis, eget tristique ante. Aenean non arcu ultrices, porta est eu, semper lorem. Vivamus dictum velit odio, ut cursus massa laoreet lobortis. Nunc sollicitudin magna ac nulla gravida luctus. Sed urna leo, viverra sit amet porta ac, luctus malesuada magna. Morbi efficitur, risus nec ultrices finibus, ante leo rhoncus lacus, vel placerat neque nisl at tortor. In cursus gravida dignissim. Cras felis est, pharetra non ultrices eget, vehicula id sapien. Nam volutpat quam purus, id iaculis purus pulvinar ut. Morbi mollis sed diam vel vestibulum. Morbi a tortor est. Ut nec ex a nibh pulvinar ullamcorper sed at nulla.</p>'
+		};
+	});
+
+
+})();
